94713377dae6b0ad082ea955d9998f5f70d0d81d
fangqing <fangqing@53fb0500-0837-a048-9c59-efeecc2212ff>
2023-07-24 07:00:04 +0000

    写入数据

--- code/api/PDMS.Project/IServices/partsProject/Partial/Icmc_pdms_project_eplService.cs
+++ code/api/PDMS.Project/IServices/partsProject/Partial/Icmc_pdms_project_eplService.cs
@@ -11,5 +11,7 @@ namespace PDMS.Project.IServices
     {
 
         public WebResponseContent submit(SaveModel saveModel);
+
+        public WebResponseContent addEpl(Guid guid,String glno);
     }
  }
