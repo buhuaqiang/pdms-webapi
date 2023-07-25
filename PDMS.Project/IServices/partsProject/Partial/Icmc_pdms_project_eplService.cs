9b003159e94d36d760b03b224263115ff1ceac84
zhangpei <zhangpei@53fb0500-0837-a048-9c59-efeecc2212ff>
2023-07-25 08:57:01 +0000

    假epl上传

--- PDMS.Project/IServices/partsProject/Partial/Icmc_pdms_project_eplService.cs
+++ PDMS.Project/IServices/partsProject/Partial/Icmc_pdms_project_eplService.cs
@@ -5,6 +5,8 @@ using PDMS.Core.BaseProvider;
 using PDMS.Entity.DomainModels;
 using PDMS.Core.Utilities;
 using System.Linq.Expressions;
+using Microsoft.AspNetCore.Http;
+
 namespace PDMS.Project.IServices
 {
     public partial interface Icmc_pdms_project_eplService
@@ -13,6 +15,23 @@ namespace PDMS.Project.IServices
         public WebResponseContent submit(SaveModel saveModel);
 
         public WebResponseContent addEpl(Guid guid,String glno);
+
+
+        /// <summary>
+        /// 根据flag下载模版
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <returns></returns>
+        WebResponseContent DownLoadTemplateByFlag(string flag);
+        /// <summary>
+        /// 上传epl
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="flag"></param>
+        /// <param name="project_id"></param>
+        /// <returns></returns>
+        WebResponseContent UploadEpl(List<IFormFile> files, string flag, string project_id);
+
 
 
         public int getDepartCount(Object obj);
