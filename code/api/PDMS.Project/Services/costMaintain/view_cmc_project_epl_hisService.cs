5b52536555350577970bd1ab1a719742253dd541
zhangpei <zhangpei@53fb0500-0837-a048-9c59-efeecc2212ff>
2023-07-27 09:52:52 +0000

    開發清冊變更記錄

---
+++ code/api/PDMS.Project/Services/costMaintain/view_cmc_project_epl_hisService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下view_cmc_project_epl_hisService与Iview_cmc_project_epl_hisService中编写
+ */
+using PDMS.Project.IRepositories;
+using PDMS.Project.IServices;
+using PDMS.Core.BaseProvider;
+using PDMS.Core.Extensions.AutofacManager;
+using PDMS.Entity.DomainModels;
+
+namespace PDMS.Project.Services
+{
+    public partial class view_cmc_project_epl_hisService : ServiceBase<view_cmc_project_epl_his, Iview_cmc_project_epl_hisRepository>
+    , Iview_cmc_project_epl_hisService, IDependency
+    {
+    public view_cmc_project_epl_hisService(Iview_cmc_project_epl_hisRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static Iview_cmc_project_epl_hisService Instance
+    {
+      get { return AutofacContainerModule.GetService<Iview_cmc_project_epl_hisService>(); } }
+    }
+ }
