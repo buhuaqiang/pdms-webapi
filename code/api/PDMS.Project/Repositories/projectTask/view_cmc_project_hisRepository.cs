1bee1fdaa749e94c843d9f3971ad6e01bea3fd08
ming <ming@53fb0500-0837-a048-9c59-efeecc2212ff>
2023-08-07 10:24:30 +0000

    [Dev]project_his業務類

---
+++ code/api/PDMS.Project/Repositories/projectTask/view_cmc_project_hisRepository.cs
@@ -0,0 +1,24 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *Repository提供数据库操作，如果要增加数据库操作请在当前目录下Partial文件夹view_cmc_project_hisRepository编写代码
+ */
+using PDMS.Project.IRepositories;
+using PDMS.Core.BaseProvider;
+using PDMS.Core.EFDbContext;
+using PDMS.Core.Extensions.AutofacManager;
+using PDMS.Entity.DomainModels;
+
+namespace PDMS.Project.Repositories
+{
+    public partial class view_cmc_project_hisRepository : RepositoryBase<view_cmc_project_his> , Iview_cmc_project_hisRepository
+    {
+    public view_cmc_project_hisRepository(SysDbContext dbContext)
+    : base(dbContext)
+    {
+
+    }
+    public static Iview_cmc_project_hisRepository Instance
+    {
+      get {  return AutofacContainerModule.GetService<Iview_cmc_project_hisRepository>(); } }
+    }
+}
---
+++ code/api/PDMS.WebApi/Controllers/Project/Partial/view_cmc_project_hisController.cs
@@ -0,0 +1,33 @@
+/*
+ *接口编写处...
+*如果接口需要做Action的权限验证，请在Action上使用属性
+*如: [ApiActionPermission("view_cmc_project_his",Enums.ActionPermissionOptions.Search)]
+ */
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using PDMS.Entity.DomainModels;
+using PDMS.Project.IServices;
+
+namespace PDMS.Project.Controllers
+{
+    public partial class view_cmc_project_hisController
+    {
+        private readonly Iview_cmc_project_hisService _service;//访问业务代码
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        [ActivatorUtilitiesConstructor]
+        public view_cmc_project_hisController(
+            Iview_cmc_project_hisService service,
+            IHttpContextAccessor httpContextAccessor
+        )
+        : base(service)
+        {
+            _service = service;
+            _httpContextAccessor = httpContextAccessor;
+        }
+    }
+}
